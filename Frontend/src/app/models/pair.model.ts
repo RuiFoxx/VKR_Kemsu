e2ea21fa35fb877e9f883567903001be2c199bde
Unknown <agentp217@gmail.com>
2018-02-21 12:52:50 +0700

    [~] class refactoring

--- Frontend/src/app/models/pair.model.ts
+++ Frontend/src/app/models/pair.model.ts
@@ -1,21 +1,23 @@
 
 export class Pair {
-    public id:number;
     public pairnumber:number;
-    //time
     public weektype:string;
     public dayofweek:string;
     public professor:string;
     public room:string;
     public discipline:string;
+    public group:string;
 
 
     constructor() {
         this.pairnumber = 1;
-        this.weektype = "Чт";
-        this.dayofweek = "Пн";
-        this.professor = "Карабцев";
-        this.room = "2220";
-        this.discipline = "Сети";
+        this.weektype = "";
+        this.dayofweek = "";
+        this.professor = "";
+        this.room = "";
+        this.discipline = "";
+        this.group="";
     }
 }
+
+
